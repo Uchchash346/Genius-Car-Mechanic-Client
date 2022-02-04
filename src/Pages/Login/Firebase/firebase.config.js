7c1a3252ba00efa36c81d37c1e42440434354968
Uchchash346 <mru416@gmail.com>
2022-02-04 10:08:02 +0600

    Initial setup

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyACj8KKovmoFMQooKZ6OSjb4kc-An6pQpE",
+    authDomain: "genius-car-mechanic-d2a71.firebaseapp.com",
+    projectId: "genius-car-mechanic-d2a71",
+    storageBucket: "genius-car-mechanic-d2a71.appspot.com",
+    messagingSenderId: "651243460838",
+    appId: "1:651243460838:web:606c684c5b7e7194af8327"
+};
+
+export default firebaseConfig;
